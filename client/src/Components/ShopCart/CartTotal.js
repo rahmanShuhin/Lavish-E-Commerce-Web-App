a90ecf737222aaf33dd2898bbc30bca6cbaf9274
rahmanShuhin <suhin4000@gmail.com>
2021-05-30 15:09:46 +0600

    redux pre updated

--- client/src/Components/ShopCart/CartTotal.js
+++ client/src/Components/ShopCart/CartTotal.js
@@ -1,7 +1,39 @@
 import React from "react";
 
 const CartTotal = () => {
-    return <div></div>;
+    return (
+        <div className="cartTotal">
+            <h2>Cart Totals</h2>
+            <div>
+                <p>SUBTOTAL</p>
+                <p>$150</p>
+            </div>
+            <div>
+                <p>Shipping</p>
+                <p>$10</p>
+            </div>
+            {/* <div>
+                <p>SHIPPING</p>
+                <span>
+                    <span>
+                        <label htmlFor="">
+                            <input type="checkbox" name="" id="" />
+                            <p>Free Shipping</p>
+                        </label>
+                    </span>
+                </span>
+            </div> */}
+            <hr />
+            <br />
+            <div>
+                <p>Total</p>
+                <p>$160</p>
+            </div>
+            <div>
+                <button>Proceed To Checkout</button>
+            </div>
+        </div>
+    );
 };
 
 export default CartTotal;
